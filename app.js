0807dc3fe4423d4d8279a8e8fadab0354ca20b98
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-03-07 19:43:23 -0300

    subo repositorio sobre prácticas de Js

---
+++ app.js
@@ -0,0 +1,77 @@
+// let num1 = 100
+// num1 =200
+// console.log(num1);
+
+// const num2 = 1000
+// num2 = 2000
+// console.log(num2);
+
+// const persona = {
+//   edad: 24,
+//   nombre: 'Cecilia',
+//   apellido: 'Luna',
+//   fechaNacimiento: 290597,
+// }
+// console.log(persona)
+
+
+
+
+
+// console.log(persona.apellido);
+
+// //array
+// const meses = ['enero', 'febrero', 'marzo', 4, persona]
+// console.log(meses);
+
+
+
+
+
+
+
+
+
+
+
+// const comision = [
+//   {nombre: 'Cecilia'},
+//   {nombre: 'Carlos'},
+//   {nombre: 'Nicolas'},
+// ]
+// console.log(comision)
+
+// //////////////////////
+
+// const numero = 100
+// const numero2 = 101
+// if(numero <= numero2) {
+//   console.log('Sí es menor o igual')
+// } else {
+//   console.log('No es mayor')
+// }
+
+//Mala práctica
+
+// if (numero > numero2){
+//   console.log('sí, es mayor o igual')
+// } else if (numero == numero2) {
+//   console.log('es igual')
+// } else (numero < numero2){
+//   console.log('no es mayor')
+// }; NO ME SALIÓ
+
+alert('Hola Cecilia');
+
+console.warn('Cuidado');
+console.error('Error');
+console.group()
+console.log('Hola Cecilia');
+
+document.write('Texto insertado en HTML desde Js.');
+
+// const nombreUsuario = prompt('Ingrese su nombre')
+// console.log(nombreUsuario);
+
+const nombreUsuario = prompt('Ingrese nombre para insertar en HTML')
+document.write(` Hola ${nombreUsuario}`);
